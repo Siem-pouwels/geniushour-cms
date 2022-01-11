3e491d0d7318204aba39e25a4c7f219d5b33848a
Tim <83349@roc-teraa.nl>
2022-01-11 14:52:39 +0100

    Migrations

---
+++ back-end/database/migrations/2022_01_11_130701_create_projectprogress_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectprogressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projectprogress', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('finished');
+            $table->integer('amountofhours');
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->unsignedBigInteger('teachergroups_id');
+            $table->foreign('teachergroups_id')->references('id')->on('teachergroups');
+            $table->unsignedBigInteger('studentgroups_id');
+            $table->foreign('studentgroups_id')->references('id')->on('studentgroups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projectprogress');
+    }
+}
