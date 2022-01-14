541ba299a2e5d8298af269a5c6eeaff0e502b9c2
Yannick <82997@roc-teraa.nl>
2022-01-14 13:22:27 +0100

    simple layout for dashboard

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import classes from './Css/Main.module.css'
 
 function Dashboard() {
     return (
+        <>
     <h1>Dashboard</h1>
+
+    <div className={classes.OverviewDashboard}>
+        Overview of projects
+    </div>
+    <br></br>
+    <div className={classes.CommentsDashboard}>
+        Comments
+    </div>
+    <div className={classes.StatsDashboard}>
+        Some stats
+    </div>
+    </>
     )
 }
 
