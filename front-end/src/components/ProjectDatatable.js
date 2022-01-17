3296db8cd1bf784baa9a663dbe49f8fb36d74ff8
Yannick <82997@roc-teraa.nl>
2022-01-17 09:34:24 +0100

    projects table fixed

--- front-end/src/components/ProjectDatatable.js
+++ front-end/src/components/ProjectDatatable.js
@@ -4,58 +4,42 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 
-export default class ProjectDatatable extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props)
-    this.deleteProject = this.deleteProject.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.handleShow = this.handleShow.bind(this);
-    this.columns = this.props[0] && Object.keys(this.props[0]);
-    this.state = {
-      show: false,
-      lastId: 0,
-    }
+export default function ProjectDatatable({ data }) {
+  const columns = data[0] && Object.keys(data[0]);
 
-  }
-
-  handleClose = () => this.setState({ show: false});
-  handleShow = () => this.setState({ show: true});
-
-  deleteProject = () => {
-    this.setState({ lastId: 1});
-    console.log(this.state.lastId)
-    axios.post('http://localhost:8000/api/projects/delete/' + this.state.lastId)
-        .then((res) => {
-            console.log('Deleted!')
-        }).catch((error) => {
-            console.log(error)
-        })
-}
-render() {
+//   deleteProject = () => {
+//     this.setState({ lastId: 1});
+//     console.log(this.state.lastId)
+//     axios.post('http://localhost:8000/api/projects/delete/' + this.state.lastId)
+//         .then((res) => {
+//             console.log('Deleted!')
+//         }).catch((error) => {
+//             console.log(error)
+//         })
+// }
   return (
       <>
     <Table striped bordered hover variant="dark">
       <thead>
         <tr>
-          {this.props[0] && this.columns.map((heading) => <th>{heading}</th>)}
+          {data[0] && columns.map((heading) => <th>{heading}</th>)}
           <th>Edit</th>
-          <th>Delete</th>
+          {/* <th>Delete</th> */}
         </tr>
       </thead>
       <tbody>
-        {this.props.map((row) => (
+        {data.map((row) => (
           <tr>
-            {this.columns.map((column) => (
+            {columns.map((column) => (
               <td>{row[column]}</td>
             ))}
             <td><Link to={`/projects/edit/${row["id"]}`}><Button variant="warning" value={row['id']}>Edit</Button></Link></td>
-            <td><Button variant="danger" value={row['id']} onClick={this.deleteProject}>Delete</Button></td>
+            {/* <td><Button variant="danger" value={row['id']} onClick={deleteProject}>Delete</Button></td> */}
           </tr>
         ))}
       </tbody>
     </Table>
-    <Modal show={this.state.show} onHide={this.handleClose}>
+    {/* <Modal show={this.state.show} onHide={this.handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
@@ -68,8 +52,7 @@ render() {
             No
           </Button>
         </Modal.Footer>
-      </Modal>
+      </Modal> */}
     </>
   );
-            }
 }
