d508b017718a10ac31e3603387d10a77a984112f
Tim <83349@roc-teraa.nl>
2022-01-21 10:58:31 +0100

    Groups and start project progress

--- back-end/app/Http/Controllers/ProjectsProgressController.php
+++ back-end/app/Http/Controllers/ProjectsProgressController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ProjectProgress;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProjectsProgressController extends Controller
 {
@@ -11,4 +12,38 @@ class ProjectsProgressController extends Controller
     {
         return response()->json(ProjectProgress::get());
     }
+
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'finished' => 'required|string',
+            'amountofhours' => 'required|string',
+            'title' => 'required|string',
+            'description' => 'required|string'
+        ]);
+
+        // if the validator fails return 400 bad request
+
+        if ($validator->fails()) {
+
+            $errors = $validator->errors();
+
+            return response()->json($errors, 400);
+
+        }
+
+        $Group = ProjectProgress::create([
+            'finished' => $request['finished'],
+            'amountofhours' => $request['amountofhours'],
+            'title' => $request['title'],
+            'description' => $request['description']
+        ]);
+
+        $response = [
+            'project' => $Group
+        ];
+
+        return response($response, 201);
+    }
+
 }
